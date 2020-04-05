4b55812a032829e221cc4a4461aafe6caa4f2ed3
unknown <kirmiziemincan@gmail.com>
2020-04-05 15:26:05 +0300

    jscrambler added

--- src/Map/Corona-Count/Corona-Count.js
+++ src/Map/Corona-Count/Corona-Count.js
@@ -168,6 +168,7 @@ export default class CoronaCount extends React.Component {
                         this.setState({ totalCountryCorona: this.state.totalCountryCorona })
                     }
                 }
+                return o;
             }
         )
     }
--- src/Map/Legend/Legend.js
+++ src/Map/Legend/Legend.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Legend.css';
 
-export default class Map extends React.Component {
+export default class Legend extends React.Component {
 
     render() {
         return (
